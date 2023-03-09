cfd367015ac516d751acdb05e47847230bf6582c
VivienRoss <vivien.rossignon@gmail.com>
2023-03-09 23:50:28 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,3 +25,7 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
+# import data from json
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# create table
+streamlit.dataframe(fruityvice_normalized)
