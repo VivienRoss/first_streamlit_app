a3128c54c2f5cda360ab60170de37e71f9d61088
VivienRoss <vivien.rossignon@gmail.com>
2023-03-10 15:27:31 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,3 +40,7 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
  
+#New section to display fruity api response
+streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like to add?','jackfruit') 
+streamlit.write('Thanks for adding',fruit_choice)
