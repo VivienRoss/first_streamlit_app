8740e173064855678bc95493db4e070e77bbfeaa
VivienRoss <vivien.rossignon@gmail.com>
2023-10-18 10:12:12 +0200

    Update streamlit_app2.py

--- streamlit_app2.py
+++ streamlit_app2.py
@@ -11,7 +11,7 @@ col3.metric("test", "86%", "4%")
 col4.metric("abc", "8.655", "-2%")
 
 
-chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["Ventes 2023", "Ventes 2022", "Target"])
+chart_data = pd.DataFrame(np.random.randn(20, 2), columns=["Ventes 2023", "Ventes 2022"])
 
 st.line_chart(chart_data)
 
@@ -19,6 +19,6 @@ df = pd.DataFrame(np.random.randn(10, 5), columns=("col %d" % i for i in range(5
 
 st.table(df)
 
-chart_data = pd.DataFrame(np.random.randn(5, 3), columns=["Produit"])
+chart_data = pd.DataFrame(np.random.randn(5, 1), columns=["Produit"])
 
 st.bar_chart(chart_data)
