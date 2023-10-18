4e38f3fe4e30b76eaaa5d398e3d1b42c4ae711fb
VivienRoss <vivien.rossignon@gmail.com>
2023-10-18 14:25:10 +0200

    Update streamlit_app2.py

--- streamlit_app2.py
+++ streamlit_app2.py
@@ -4,9 +4,13 @@ import numpy as np
 import altair as alt
 import streamlit as st
 
-tab1, tab2 = st.tabs(["📈 Overview", "🗃 Details"])
+tab1, tab2, tab3 = st.tabs(["📈 Overview", "🗃 Details", "Other visuals"])
 
 tab1.subheader("Sales Overview") 
+
+tab1.text('Le Lorem Ipsum est simplement du faux texte employé dans la composition')
+tab1.text('et la mise en page avant impression.')
+tab1.markdown("""---""")
 
 col1, col2, col3, col4 = tab1.columns(4)
 col1.metric("Total Sales", "52.636 €", "5,2 %")
@@ -22,8 +26,6 @@ tab1.bar_chart(
    chart_data, x="Produits", y=["CY", "LY"], color=["#FF0000", "#0000FF"]  
 ) 
 
-tab1.text('Le Lorem Ipsum est simplement du faux texte employé dans la composition')
-tab1.text('et la mise en page avant impression.')
 
 
 tab2.subheader("Sales Details")
@@ -36,4 +38,8 @@ df = pd.DataFrame(np.random.randn(12, 5), columns=(["Produit 1","Produit 2","Pro
 
 tab2.table(df)
 
+tab3.subheader("Other visuals")
 
+chart_data = pd.DataFrame({"Produits":["Produit 1","Produit 2","Produit 3","Produit 4","Produit 5"], "CY": np.random.randn(5),"LY": np.random.randn(5)})
+
+tab3.scatter_chart(chart_data)
