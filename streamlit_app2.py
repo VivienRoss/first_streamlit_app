8e2431ec305c339f2c882951c47e12cdfed11dea
VivienRoss <vivien.rossignon@gmail.com>
2023-10-18 10:19:26 +0200

    Update streamlit_app2.py

--- streamlit_app2.py
+++ streamlit_app2.py
@@ -9,7 +9,7 @@ tab1, tab2 = st.tabs(["📈 Vue d'ensemble", "🗃 Details"])
 tab1.subheader("Information des ventes du jour pour le magasin XX") 
 
 col1, col2, col3, col4 = st.columns(4)
-tab1.metric("Total des Ventes", "52.636 €", "5,2 %")
+col1.tab1.metric("Total des Ventes", "52.636 €", "5,2 %")
 tab1.metric("# Tickets ", "522", "-8%")
 tab1.metric("test", "86%", "4%")
 tab1.metric("abc", "8.655", "-2%")
