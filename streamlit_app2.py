ff4dedc0dd9f10e95c27435d330dfa4a0506c692
VivienRoss <vivien.rossignon@gmail.com>
2023-10-18 10:17:59 +0200

    Update streamlit_app2.py

--- streamlit_app2.py
+++ streamlit_app2.py
@@ -9,23 +9,23 @@ tab1, tab2 = st.tabs(["📈 Vue d'ensemble", "🗃 Details"])
 tab1.subheader("Information des ventes du jour pour le magasin XX") 
 
 col1, col2, col3, col4 = st.columns(4)
-col1.metric("Total des Ventes", "52.636 €", "5,2 %")
-col2.metric("# Tickets ", "522", "-8%")
-col3.metric("test", "86%", "4%")
-col4.metric("abc", "8.655", "-2%")
+tab1.col1.metric("Total des Ventes", "52.636 €", "5,2 %")
+tab1.col2.metric("# Tickets ", "522", "-8%")
+tab1.col3.metric("test", "86%", "4%")
+tab1.col4.metric("abc", "8.655", "-2%")
 
 chart_data = pd.DataFrame(np.random.randn(5, 1), columns=["Produit"])
 
-st.bar_chart(chart_data)
+tab1.st.bar_chart(chart_data)
 
-tab1.subheader("Details des ventes")
+tab2.subheader("Details des ventes")
 
 chart_data = pd.DataFrame(np.random.randn(20, 2), columns=["Ventes 2023", "Ventes 2022"])
 
-st.line_chart(chart_data)
+tab2.st.line_chart(chart_data)
 
 df = pd.DataFrame(np.random.randn(10, 5), columns=("col %d" % i for i in range(5)))
 
-st.table(df)
+tab2.st.table(df)
 
 
