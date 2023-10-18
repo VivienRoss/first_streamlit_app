75d7d73e054579b798c2944d7d23a1f08e9d7227
VivienRoss <vivien.rossignon@gmail.com>
2023-10-18 10:14:53 +0200

    Update streamlit_app2.py

--- streamlit_app2.py
+++ streamlit_app2.py
@@ -4,12 +4,23 @@ import numpy as np
 
 import streamlit as st
 
+tab1, tab2 = st.tabs(["Overview", "Details"])
+
+with tab1:
+   st.header("Information des ventes du jour pour le magasin XX")
+  
 col1, col2, col3, col4 = st.columns(4)
 col1.metric("Total des Ventes", "52.636 €", "5,2 %")
 col2.metric("# Tickets ", "522", "-8%")
 col3.metric("test", "86%", "4%")
 col4.metric("abc", "8.655", "-2%")
 
+chart_data = pd.DataFrame(np.random.randn(5, 1), columns=["Produit"])
+
+st.bar_chart(chart_data)
+
+with tab2:
+   st.header("Details des ventes")
 
 chart_data = pd.DataFrame(np.random.randn(20, 2), columns=["Ventes 2023", "Ventes 2022"])
 
@@ -19,6 +30,4 @@ df = pd.DataFrame(np.random.randn(10, 5), columns=("col %d" % i for i in range(5
 
 st.table(df)
 
-chart_data = pd.DataFrame(np.random.randn(5, 1), columns=["Produit"])
 
-st.bar_chart(chart_data)
