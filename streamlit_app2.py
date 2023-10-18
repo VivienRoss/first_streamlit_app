720c95a032c8f10f45cef724de7b8034d68226f0
VivienRoss <vivien.rossignon@gmail.com>
2023-10-18 14:18:31 +0200

    Update streamlit_app2.py

--- streamlit_app2.py
+++ streamlit_app2.py
@@ -22,6 +22,9 @@ tab1.bar_chart(
    chart_data, x="Produits", y=["CY", "LY"], color=["#FF0000", "#0000FF"]  
 ) 
 
+tab1.text('Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.')
+
+
 tab2.subheader("Sales Details")
 
 chart_data = pd.DataFrame(np.random.randn(12, 2), columns=["Sales 2023", "Sales 2022"])
